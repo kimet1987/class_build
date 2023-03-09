e9dd65eaaaab74177e50bd8696baa28c755b65cd
byoungsu kim <kimet2001@gmail.com>
2023-03-09 11:02:34 +0900

    커밋

--- next.config.js
+++ next.config.js
@@ -2,6 +2,14 @@
 const nextConfig = {
     reactStrictMode: true,
     trailingSlash: true,
+    generateBuildId: () => "codecapm-deploy-project",
+
+    // 아래 주소만 out폴더에 만들어줘 (getServerSideProps 있는 패이지는 제외)
+    exportPathMap: () => ({
+        "/": { page: "/" },
+        "/boards": { page: "/boards" },
+        "/404": { page: "/404" },
+    }),
 };
 
 module.exports = nextConfig;
--- pages/boards/[boardId]/index.js
+++ pages/boards/[boardId]/index.js
@@ -9,3 +9,8 @@ export default function BoardsPage(params) {
         </>
     );
 }
+
+// export const getServerSideProps = () => {
+//     // 만약 서바사이드 렌더링을 하는 페이지라면? ==> out 폴더로 생성 불가
+//     // 이런 경우는 nest.config.js 에서 exportPathMap으로 현재 페이지 제외 시키기
+// };
